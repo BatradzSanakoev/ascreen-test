2d164885e58070f04057a11528c2196ff5c3cd31
WORK\bsanakoev <bsanakoev@kodeks.ru>
2021-05-19 17:47:27 +0300

    feat: реализовать все условия ТЗ

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import { Route, Link, useLocation } from 'react-router-dom';
 
@@ -6,13 +7,47 @@ import MountainPage from '../MountainPage/MountainPage'
 
 export default function Main() {
     const location = useLocation();
+
     return (
         <>
+            {/* Каждая "Гора" представлена в виде ссылки  */}
             <main className={`${location.pathname === '/' ? 'main' : 'visible'}`}>
-                <Link to='/mountains' className='link'/>
+                <Link to='/mountains/m1' className='mountain-link first' />
+                <Link to='/mountains/m2' className='mountain-link second' />
+                <Link to='/mountains/m3' className='mountain-link third' />
+                <Link to='/mountains/m4' className='mountain-link fourth' />
+                <Link to='/mountains/m5' className='mountain-link fifth' />
+                <Link to='/mountains/m6' className='mountain-link sixth' />
+                <Link to='/mountains/m7' className='mountain-link seventh' />
+                <Link to='/mountains/m8' className='mountain-link eighth' />
             </main>
-            <Route path='/mountains'>
-                <MountainPage name={mountains.name} images={mountains.images} text={mountains.text} />
+
+            {/* Расписанные маршруты для перехода на страницы каждой "Горы". При переходе на ту или иную страницу компоненту MountainPage передаются данные "Горы"
+             В данном случае из-за малого кол-ва данных все компоненты и маршруты прописаны вручную, но при большем количестве маршрутов можно будет все заполнить, 
+             используя, к примеру цикл */}
+            <Route path='/mountains/m1'>
+                <MountainPage id={mountains.m1.id} name={mountains.m1.name} images={mountains.m1.images} text={mountains.m1.text} />
+            </Route>
+            <Route path='/mountains/m2'>
+                <MountainPage id={mountains.m2.id} name={mountains.m2.name} images={mountains.m2.images} text={mountains.m2.text} />
+            </Route>
+            <Route path='/mountains/m3'>
+                <MountainPage id={mountains.m3.id} name={mountains.m3.name} images={mountains.m3.images} text={mountains.m3.text} />
+            </Route>
+            <Route path='/mountains/m4'>
+                <MountainPage id={mountains.m4.id} name={mountains.m4.name} images={mountains.m4.images} text={mountains.m4.text} />
+            </Route>
+            <Route path='/mountains/m5'>
+                <MountainPage id={mountains.m5.id} name={mountains.m5.name} images={mountains.m5.images} text={mountains.m5.text} />
+            </Route>
+            <Route path='/mountains/m6'>
+                <MountainPage id={mountains.m6.id} name={mountains.m6.name} images={mountains.m6.images} text={mountains.m6.text} />
+            </Route>
+            <Route path='/mountains/m7'>
+                <MountainPage id={mountains.m7.id} name={mountains.m7.name} images={mountains.m7.images} text={mountains.m7.text} />
+            </Route>
+            <Route path='/mountains/m8'>
+                <MountainPage id={mountains.m8.id} name={mountains.m8.name} images={mountains.m8.images} text={mountains.m8.text} />
             </Route>
         </>
     )
--- src/components/MountainPage/MountainPage.js
+++ src/components/MountainPage/MountainPage.js
@@ -1,42 +1,62 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import mselected from '../../images/mselected.png';
-import m from '../../images/m.png';
-import back from '../../images/back.png';
-import full from '../../images/full.png';
+import selIcon from '../../images/mselected.png';
+import icon from '../../images/m.png';
 import ball from '../../images/ball.png';
 import ballSel from '../../images/ballSel.png';
 
-export default function MountainPage({ name, images, text }) {
+export default function MountainPage({ id, name, images, text }) {
+    // Стейт переменная для смены состояния кнопки слайдера
+    const [selected, setSelected] = React.useState(0);
+
+    // Метод отображения картинки в полноэкранном режиме
+    const handleImgClick = () => {
+        document.querySelector('.mountainPage__image').classList.toggle('fullscreen');
+        document.querySelector('.mountainPage__info').classList.toggle('visible');
+    };
+
+    // Метод работы слайдера
+    const handleSlider = (num) => {
+        document.querySelector('.image').src = images[num];
+        setSelected(num);
+    };
+
     return (
         <div className='mountainPage'>
             <div className='mountainPage__image'>
-                <img src={images[0]} className='image' />
+                <img src={images[0]} className='image' alt='main-wallpaper' />
                 <div className='image__down'>
                     <Link to='/' className='back' />
                     <div className='image__slider'>
-                        <img src={ballSel} />
-                        <img src={ball} />
-                        <img src={ball} />
+                        {/* Как и в компоненте Main, здесь все кнопки слайдера прописаны вручную из-за малого количества данных
+                     При большой выборке можно будет процесс автоматизировать, использовав, например, цикл                     */}
+                        <img src={selected === 0 ? ballSel : ball} alt='controls' onClick={() => handleSlider(0)} />
+                        <img src={selected === 1 ? ballSel : ball} alt='controls' onClick={() => handleSlider(1)} />
+                        <img src={selected === 2 ? ballSel : ball} alt='controls' onClick={() => handleSlider(2)} />
                     </div>
-                    <Link to='/' className='full' />
+                    <Link to='#' className='full' onClick={handleImgClick} />
                 </div>
             </div>
             <div className='mountainPage__info'>
                 <div className='mountainPage__text'>
                     <h2 className='text__name'>{name}</h2>
-                    <p className='text__paragraph'>{text[0]}</p>
-                    <p className='text__paragraph'>{text[1]}</p>
-                    <div className='text__img'>
-                        <img src={mselected} />
-                        <img src={m} />
-                        <img src={m} />
-                        <img src={m} />
-                        <img src={m} />
-                        <img src={m} />
-                        <img src={m} />
-                        <img src={m} />
+                    <div className='text'>
+                        <p className='text__paragraph'>{text[0]}</p>
+                        <p className='text__paragraph'>{text[1]}</p>
+                    </div>
+                    <div className='text__icons'>
+                        {/* Тут также как и ранее все иконки гор написаны вручную из-за малого количества данных, 
+                         при большем количестве вывод можно реализовать использовав, например, метод map */}
+                        <img src={id === 1 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 2 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 3 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 4 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 5 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 6 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 7 ? selIcon : icon} alt='mountain-icon' />
+                        <img src={id === 8 ? selIcon : icon} alt='mountain-icon' />
                     </div>
                 </div>
             </div>
